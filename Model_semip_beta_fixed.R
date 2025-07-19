f24dbf152f230e67f20a918b794ca62498372422
Christian Caamaño Carrillo <chcaaman@ubiobio.cl>
2025-07-18 22:27:58 -0400

    Update Model_semip_beta_fixed.R

--- Model_semip_beta_fixed.R
+++ Model_semip_beta_fixed.R
@@ -2,17 +2,12 @@
 ### Estimation Semiparametric additive beta regression model with fixed     ###
 ### precision parameter                                                     ###
 ### ======================================================================= ###
-model.fixed <- function(X,T1,Y,lambda1){
+  model.fixed <- function(X,T1,Y,lambda1){
   
   T1_0 <- sort(T1[!duplicated(T1)])
   n <- length(Y)
   r1 <- length(T1_0)
-  c <- length(Y)
-  CONS <- numeric(c)
-  for (i in 1:c) {
-    CONS[i] <- 1
-  }
-  W <- cbind(CONS,X)
+  W <- cbind(1,X)
   p <- length(W[1,])
   
   ############### Incidence matrix ##################
@@ -42,11 +37,10 @@ model.fixed <- function(X,T1,Y,lambda1){
       }
     }
   }
-  
   #### Matrix K1
   K1 <- Q1%*%solve(R1)%*%t(Q1)
   
-  #### Matrix N1
+  #### Matriz N1
   N1 <- array(0, dim=c(n,r1))
   for(j in 1:r1){
     for(i in 1:n){
@@ -77,10 +71,10 @@ model.fixed <- function(X,T1,Y,lambda1){
   Alfa_0n <- solve(t(WW)%*%WW)%*%t(WW)%*%(as.matrix(Y))                     
   eta_0n <- WW%*%Alfa_0n + NN1%*%F1_0n  
   
-  sum_1 <-(NN1%*%F1_0n)
-  sum_3 <-(WW%*%Alfa_0n +sum_1)
-  sum_4 <- exp(sum_3)
-  ff <- sum_4
+  sum1 <-(NN1%*%F1_0n)
+  sum3 <-(WW%*%Alfa_0n +sum1)
+  sum4 <- exp(sum3)
+  ff <- sum4
   ff1 <- 1+ff
   mu <- ff/(ff1)
   
@@ -89,6 +83,7 @@ model.fixed <- function(X,T1,Y,lambda1){
   c <- log(gamma((1-mu)*fi_0n))
   L_0n <- sum(a-b-c+((mu*fi_0n)-1)*log(Y)+((1-mu)*fi_0n-1)*log(1-Y))-0.5*lambda1*t(F1_0n)%*%K1%*%F1_0n
   L_0n
+  
   
   #### Matrix T
   gg_mu <- log(mu/(1-mu))
@@ -100,7 +95,7 @@ model.fixed <- function(X,T1,Y,lambda1){
   M_0n <- diag(as.vector(w_i))
   
   #### Definition of Z
-  y_i <- log(Y/(1-Y))
+   y_i <- log(Y/(1-Y))
   u_0n <- digamma(mu*fi_0n)-digamma((1-mu)*fi_0n)
   z_0n <- eta_0n + solve(M_0n)%*%T_0n%*%(y_i-u_0n)
   e <- y_i-eta_0n
@@ -118,12 +113,12 @@ model.fixed <- function(X,T1,Y,lambda1){
   z_in <- z_0n
   L_in <- L_0n
   
-  norma_TETA <- 10 ; epsilon_TETA <- 10^-5
-  norma_ALFA <- 10 ; epsilon_ALFA <- 10^-5
-  norma_F1 <- 10 ; epsilon_F1 <- 10^-5
+  norm_TETA <- 10 ; epsilon_TETA <- 10^-5
+  norm_ALFA <- 10 ; epsilon_ALFA <- 10^-5
+  norm_F1 <- 10 ; epsilon_F1 <- 10^-5
   
-  while (norma_TETA >= epsilon_TETA){
-    while(norma_ALFA >= epsilon_ALFA  & norma_F1 >= epsilon_F1){
+  while (norm_TETA >= epsilon_TETA){
+    while(norm_ALFA >= epsilon_ALFA  & norm_F1 >= epsilon_F1){
       
       S0_n <- solve(t(WW)%*%M_in%*%WW)%*%t(WW)%*%M_in
       S1_n <- solve(t(NN1)%*%M_in%*%NN1+lambda1*fi_in*K1)%*%t(NN1)%*%M_in
@@ -131,16 +126,16 @@ model.fixed <- function(X,T1,Y,lambda1){
       ALFA_en <- S0_n%*%(z_in - NN1%*% F1_in)
       F1_en <- S1_n%*%(z_in - WW%*%ALFA_en)
       
-      norma_ALFA <- sqrt(t(ALFA_en - ALFA_in)%*%(ALFA_en - ALFA_in)/(t(ALFA_in)%*%ALFA_in))
-      norma_F1 <- sqrt(t(F1_en-F1_in)%*%(F1_en-F1_in)/(t(F1_in)%*%F1_in))
+      norm_ALFA <- sqrt(t(ALFA_en - ALFA_in)%*%(ALFA_en - ALFA_in)/(t(ALFA_in)%*%ALFA_in))
+      norm_F1 <- sqrt(t(F1_en-F1_in)%*%(F1_en-F1_in)/(t(F1_in)%*%F1_in))
       
       ALFA_in <- ALFA_en
       F1_in <- F1_en
       
       eta_en <- WW%*%ALFA_in + NN1%*%F1_in 
-      sum_11 <- (NN1%*%F1_in)
-      sum_33 <- exp(WW%*%ALFA_in + sum_11)
-      fff <- sum_33
+      sum11 <- (NN1%*%F1_in)
+      sum33 <- exp(WW%*%ALFA_in + sum11)
+      fff <- sum33
       ff11 <- 1+fff
       mu_en <- fff/(ff11)
       
@@ -152,7 +147,8 @@ model.fixed <- function(X,T1,Y,lambda1){
       e1 <- (y_i - eta_en)
       sigma1 <- (as.numeric(t(e1)%*%e1)/((n-p)*(g_mu1^2)))
       fi_en <- (sum((mu_en-mu_en^2)/sigma1)-1)/n
-      
+
+
       # Matrix W
       w_1  <- fi_en*(trigamma(mu_en*fi_en)+trigamma((1-mu_en)*fi_en))*((1/(g_mu1))^2)
       M_en <- diag(as.vector(w_1))  
@@ -171,19 +167,29 @@ model.fixed <- function(X,T1,Y,lambda1){
     b1 <- log(gamma(mu_en*fi_en))
     c1 <- log(gamma((1-mu_en)*fi_en))
     L_en <- sum(a1-b1-c1+((mu_en*fi_en)-1)*log(Y)+((1-mu_en)*fi_en-1)*log(1-Y))-0.5*lambda1*t(F1_in)%*%K1%*%F1_in 
-    norma_TETA <- abs((L_in-L_en)/(L_en))
+    norm_TETA <- abs((L_in-L_en)/(L_en))
     L_in <- L_en
   }
   
   ############## Effective degrees of freedom ############ 
   edf_alpha1 <- sum(diag(NN1%*%S1_n))
-  edf_alpha1
+  edf_alpha1  
   
   ############ Akaike information criterion AIC ###########
-  AIC_alpha1 <- -2*L_en + 2*(p + 1 + edf_alpha1)
-  
+  AIC_alpha1 <- -2*L_en + 2*(p + 1+ edf_alpha1)
+
+  ############ Bayesian information criterion AIC ###########
+  BIC_alpha1 <- -2*L_en + log(n)*(p+1  +  edf_alpha1  )
+
+  ############ Pseudo R2 ###########
+  edf_total <- p + edf_alpha1
+  SQR1 <- sum((Y - mu_en)^2)/(n-edf_total)
+  SQT1 <- sum((Y - mean(Y))^2)/(n-1)
+  R2 <- 1 - (SQR1 / SQT1)
+  R2_adj<- 1 - ((1 - R2)*(n - 1)) / (n - edf_total )
+
   #################################### Hessian ###################################
-  
+
   ## Hessian
   Q <- diag(as.vector((fi_in*(trigamma(mu_en*fi_in)+trigamma((1-mu_en)*fi_in))+(y_1 - u_en)*((1-2*mu_en)/(mu_en-mu_en^2)))*((mu_en-mu_en^2))^2))
   bb <- (y_1-u_en)-fi_in*((trigamma(mu_en*fi_in))*mu_en-trigamma((1-mu_en)*fi_in)*(1-mu_en))*(mu_en-mu_en^2)
@@ -198,7 +204,7 @@ model.fixed <- function(X,T1,Y,lambda1){
   H41 <- t(t(WW)%*%bb)
   H42 <- t(t(NN1)%*%bb)
   H44 <- sum(diag(D))
-  H_TETAn <- rbind(cbind(H11,H12,H14),cbind(H21,H22,H24),cbind(H41,H42,H44))      
+  H_TETAn <- rbind(cbind(H11,H12,H14),cbind(H21,H22,H24),cbind(H41,H42,H44))
   
   ## Fisher information matrix
   
@@ -227,11 +233,31 @@ model.fixed <- function(X,T1,Y,lambda1){
   SD_ALFA_n <- SE_tetan[1:p]
   SD_F1_n <- SE_tetan[(p+1):(p+r1)]
   SD_fi_n <- SE_tetan[p+r1+1]
+
+  ## GCV
+  H <- sqrt(M_en)%*%WW%*%solve(t(WW)%*%M_en%*%WW)%*%t(WW)%*%sqrt(M_en)
+  #  GCV <- sum((Y-mu_en)^2)/(n*(1-sum(diag(H))/n)^2)
+  GCV <- sum((Y-mu_en)^2)/(n*(1-(edf_alpha1+p+1)/n)^2)
   
-  return(list(AIC=AIC_alpha1,edf_func1=edf_alpha1,beta=ALFA_en
-              ,F1_en=F1_en,phi=fi_en,SD_beta=SD_ALFA_n,SD_F1_n=SD_F1_n,SD_phi=SD_fi_n))
+ return(list(AIC=AIC_alpha1,BIC=BIC_alpha1,GCV=GCV,edf_func1=edf_alpha1,beta=ALFA_en,Like=L_en,
+            F1_en=F1_en,phi=fi_en,SD_beta=SD_ALFA_n,SD_F1_n=SD_F1_n,SD_phi=SD_fi_n,R2_adj=R2_adj))
 
 }
+
+
+
+
+
+
+
+ 
+    
+    
+
+
+
+
+
 
 
 
